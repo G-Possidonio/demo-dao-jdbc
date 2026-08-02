6f66b4d13dd5d7d339a12f23710f41c7581ef861
G-Possidonio <g.possidonio00@gmail.com>
2026-08-02 17:04:41 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +16,9 @@ public class Program {
 		
 		Seller sel = new Seller(1,"Gs", "Gs@gmail.com", new Date(), 3000.0, obj);
 		System.out.println(sel);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 	}
 
 }
