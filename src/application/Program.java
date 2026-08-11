6b02612b5a3861ddb1c03fb226964642dd049d4a
G-Possidonio <g.possidonio00@gmail.com>
2026-08-10 22:11:41 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,19 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Department obj = new Department(1, "Teste");
-		System.out.println(obj);
-		
-		Seller sel = new Seller(1,"Gs", "Gs@gmail.com", new Date(), 3000.0, obj);
-		System.out.println(sel);
-		
+			
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 		
 	}
 
